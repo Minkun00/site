d559ab60ffc7fddc5a8d5bdd7e2834cde584df95
Minkun <139968456+Minkun00@users.noreply.github.com>
2024-01-06 00:23:58 +0900

    Update FullPosts.jsx

--- src/components/FullPosts/FullPosts.jsx
+++ src/components/FullPosts/FullPosts.jsx
@@ -65,7 +65,7 @@ export default function FullPost() {
     const confirmation = window.confirm('Do you really want to delete this post?');
 
     if (confirmation) {
-      await deletePost(post, globalState, post.contentNum);
+      await deletePost(post, globalState);
       navigate('/site');
     }
   };
